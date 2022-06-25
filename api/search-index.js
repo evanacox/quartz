e8f73e2814829d63353b9240d885b2a07e0f5b33
evanacox <evanacox@users.noreply.github.com>
2022-06-25 20:46:54 +0000

    deploy: fa2dfda2ae95c9778b42afd5f56b411ffcf2de4c

---
+++ api/search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"quartz":{"doc":"Quartz","t":[5],"n":["add_two"],"q":["quartz"],"d":["Adds two numbers together."],"i":[0],"f":[[[["i32",0],["i32",0]],["i32",0]]],"p":[]}\
+}');
+if (window.initSearch) {window.initSearch(searchIndex)};
---
+++ api/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["quartz"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
