94c49d47de98cfcef5cc6045475bbfa1b471447a
Evan Cox <evanacox00@gmail.com>
2022-06-25 17:19:36 -0400

    fails tests

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -35,7 +35,13 @@
 /// assert_eq!(result, 4);
 /// ```
 pub fn add_two(x: i32, y: i32) -> i32 {
+    let z = 5;
+
     x + y
+}
+
+fn fails_test() -> i32 {
+    32
 }
 
 #[cfg(test)]
@@ -44,8 +50,11 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add_two(2, 2);
+        assert_eq!(add_two(2, 2), 4);
+    }
 
-        assert_eq!(result, 4);
+    #[test]
+    fn should_work() {
+        assert_eq!(fails_test(), 31);
     }
 }
